4ae361dfcb79f65809007ad11cad8447764befd4
Purvansh <purvansh.23bcs10169@ms.sst.scaler.com>
2024-07-10 11:24:45 +0530

    Used Bcrypt

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,37 +1,51 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt')
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
     try {
-
+        const userExists = await User.findOne({email:req.body.email})
+        if(userExists){
+            res.send({
+                success : false,
+                message : "User Already Exists"
+            })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password , salt)
+        req.body.password = hashedPassword
         const newUser = new User(req.body)
         await newUser.save()
 
         res.status(201).json('User Created')
-
     } catch (error) {
         res.json(error)
     }
 });
 
 router.post("/login", async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username });
-        if (!user) {
-            res.status(404).json({ message: "User not found" });
-            return;
+    const user = await User.findOne({email : req.body.email})
+    if(!user){
+        res.send({
+            success : false,
+            message : "User Not Found"
+        })
+    }
+    if(user){
+        const validPassword = await bcrypt.compare(req.body.password , user.password)
+        if(validPassword){
+            res.send({
+                success : true,
+                message : "User Logged In"
+            })
+        }else{
+            res.send({
+                success : false,
+                message : "Invalid Password"
+            })
         }
-        const isPasswordValid = await user.comparePassword(password);
-        if (!isPasswordValid) {
-            res.status(401).json({ message: "Invalid password" });
-            return;
-        }
-        res.status(200).json({ message: "Login successful" });
-    } catch (error) {
-        res.status(500).json({ message: "Failed to login" });
     }
 });
 
